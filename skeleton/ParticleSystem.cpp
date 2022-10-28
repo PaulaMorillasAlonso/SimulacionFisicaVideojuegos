ee63d252b492e52ce9126387d2031a459cf2b8b8
PaulaMorillasAlonso <pamorill@ucm.es>
2022-10-28 10:07:19 +0200

    act

--- skeleton/ParticleSystem.cpp
+++ skeleton/ParticleSystem.cpp
@@ -5,7 +5,7 @@ ParticleSystem::ParticleSystem()
 	/*fuente_ = new UniformParticleGenerator({ 7,50,7 }, { 0,0,0 }, {0,-10.0,0}, 2, 5, 1, 2, 1, 1, 0.99, 5000, { 0,0,1,1 },1.0);
 	_particle_generators.push_back(fuente_);*/
 	
-	/*humo_ = new GaussianParticleGenerator({ 7,50,7 }, { 7,7,2 }, { 0,2,0 }, {4, 4, 0}, { 2,2,0 }, 1, 1, 0.99, 3000, { 0.8,0.8,0.8,1 });
+	/*humo_ = new GaussianParticleGenerator({ 7,50,7 }, { 5,5,2 }, { 0,2,0 }, {4, 4, 0}, { 2,2,0 }, 1, 1, 0.99, 3000, { 0.8,0.8,0.8,1 },0.8);
 	_particle_generators.push_back(humo_);*/
 	createFireworkRules();
 }
@@ -71,9 +71,9 @@ void ParticleSystem::createFireworkRules()
 {
 	firework_rules_ = std::vector<FireworkRule>(3);
 
-	firework_rules_[0].set({ 7,50,7 }, { 2,2,1 }, { 0,4.0,0 }, 0.99, 3000, randomColour(), 1.0, 20, 0); //Fuego básico, sube y explota
-	firework_rules_[1].set({ 10,40,10 }, { 2,2,0 }, { 0,4.0,0 }, 0.99, 3000, randomColour(), 1.0, 10, 1); //Fuego circular
-	firework_rules_[2].set({ 15,60,15 }, { 2,2,0 }, { 0,4.0,0 }, 0.99, 3000, randomColour(),0.5,30,2); //Fuego esfera
+	firework_rules_[0].set({ 7,50,7 }, { 10,10,1 }, { 0,8.0,0 }, 0.99, 1000, randomColour(), 0.3, 40, 0); //Fuego básico, sube y explota
+	firework_rules_[1].set({ 10,40,10 }, { 10,10,0 }, { 0,8.0,0 }, 0.99, 2000, {0.1,0.9,0.9,1}, 0.5, 50, 1); //Fuego circular
+	firework_rules_[2].set({ 15,60,15 }, { 10,10,0 }, { 0,8.0,0 }, 0.99, 1000, {0.9,0.1,0.47,1}, 0.3, 60, 2); //Fuego esfera
 
 }
 void ParticleSystem::generateFireworkSystem(unsigned type)
@@ -96,7 +96,7 @@ void ParticleSystem::generateFireworkSystem(unsigned type)
 		/*g = std::shared_ptr<ParticleGenerator>(new UniformParticleGenerator(firework_rules_[type].pos_, firework_rules_[type].vel_,firework_rules_[type].acc_,
 			1, 4,0, 5, 1, firework_rules_[type].payload_, firework_rules_[type].damping_, firework_rules_[type].lifeTime_,
 			randomColour(), firework_rules_[type].size_));*/
-		g = std::shared_ptr<ParticleGenerator>(new SphereParticleGenerator({ 4,4,0 }, 1, 15, firework_rules_[type].payload_));
+		g = std::shared_ptr<ParticleGenerator>(new SphereParticleGenerator({ 10,10,0 }, 1, 15, firework_rules_[type].payload_));
 
 	}
 	
